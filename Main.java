f501e0104d86daa3b561b3db61fc32aec64a1a64
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-03 16:43:36 +0000

    Option 3 and comments

--- Main.java
+++ Main.java
@@ -15,6 +15,7 @@ class Main {
     System.out.println("Welcome to Life Simulator");
     // Print title and offer first options
     System.out.println("You wake up. What do you do? (1.Go to Shower, 2.Go to Work, 3.Go to Park)");
+    // Have user input 
     int wakeupChoice  = input.nextInt();
     if (wakeupChoice == 1){
       System.out.println("You take a shower. Now you are ready for the day.");
@@ -22,6 +23,13 @@ class Main {
     else if (wakeupChoice == 2){
       System.out.println("You go to work. Since you did not shower, you smell and are therfore fired from your job.");
     }
+    else if(wakeupChoice == 3){
+      System.out.println("You go for a walk and burn 1,000 calories and therfore a fit.");
+    }
+    else{
+      System.out.println("Input Error. Input a number between 1-3");
+      // Error Prep -> Tells user to input number between 1-3 in order to have a valid output
       
+    }
   }
 }
