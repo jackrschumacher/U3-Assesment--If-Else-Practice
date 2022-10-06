6acb80884cbe440e975c43a223ed1b716bbe4171
Jack Schumacher <84467112+jackrschumacher11@users.noreply.github.com>
2022-10-06 16:01:35 +0000

    Layer Numbers

--- Main.java
+++ Main.java
@@ -21,17 +21,21 @@ class Main {
     System.out.println(" 1. Take a shower and get ready for your day. \n 2. Go for a walk \n 3. Go for a Drive");
     // Have user input a number between 1&3 in order to create a valid output
     int wakeupChoice = input.nextInt();
+
+    // Layer #1
     if (wakeupChoice == 1) {
       System.out.println("You take a shower. Now you are ready for the day");
       System.out.println("What do you want to do today?");
       System.out
           .println(" 1. Sit around and play videogames \n 2. Go for a walk around town \n 3.Go to a new place today");
       int dayChoice = input.nextInt();
+      // Layer #2
       if (dayChoice == 1) {
         // Choice for Day #1
         System.out.println("What video games do you want to play");
         System.out.println(" 1. Mario Cart \n 2. Tetris \n 3. Runescape");
         int gameChoice = input.nextInt();
+        // Layer #3
         if (gameChoice == 1) {
           System.out.println("You play Mario cart and win the game.");
         } else if (gameChoice == 2) {
@@ -42,12 +46,14 @@ class Main {
           System.out.println("You entered an invalid input. Please enter a number between 1&3.");
           // Tell user to restart and enter number between 1&3
         }
-      } else if (dayChoice == 2) {
+      } 
+      else if (dayChoice == 2) { //Layer #2
         System.out.println("You decide to go for a walk around your town. What do you do when you are in the town?");
         System.out.println(
             " 1. Go eat Ice Cream at the town ice cream store \n 2. Go fishing in the town pond \n 3. Go get a burger from Culvers");
         // Users input selects an if statement that is true
         int townChoice = input.nextInt();
+        // Layer #3
         if (townChoice == 1) {
           System.out
               .println("You go to the town ice cream store and get a cup of mint chip ice cream. It tastes delicious.");
@@ -60,11 +66,14 @@ class Main {
           // Tell user to restart and enter number between 1&3
         }
 
-      } else if (dayChoice == 3) {
+      } 
+        // Layer #2
+      else if (dayChoice == 3) {
         System.out.println("You decide to go to a new place today. Where would you like to go?");
         System.out.println(" 1.Canada \n 2.The United Kingdom \n 3.Switzerland");
         int placeChoice = input.nextInt();
         // Users input selects an option that satisfies the if (input = 1, etc.)
+        // Layer #3
         if (placeChoice == 1) {
           System.out.println("You travel to Canda. It is very cold there.");
         } else if (placeChoice == 2) {
@@ -77,7 +86,9 @@ class Main {
 
         }
 
-      } else {
+      } 
+        
+      else {
         System.out.println("You entered an invalid input. Please enter a number between 1&3.");
         // Tell user to restart and enter number between 1&3
 
